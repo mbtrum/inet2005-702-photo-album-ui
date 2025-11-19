8e37211328c37d2447d6e6aff52e74b89b38a52d
Michael Trumbull <michael.trumbull@nscc.ca>
2025-11-19 11:25:48 -0400

    Working on pulling photos from API.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'dotenv/config'
 
+// TO-DO: Fix "process is not defined" error in the browser console
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // Define a state variable to hold photos
+    const [photos, setPhotos] = useState([])
+
+    // Get API Url from environment variables
+    const apiUrl = process.env.PHOTOS_API_URL
+
+    // Fetch photos from API when component mounts
+    useEffect(() => { 
+        const getPhotos = async () => { 
+            const response = await fetch(apiUrl) 
+            const result = await response.json() 
+
+            if(response.ok) { 
+                setPhotos(result)
+            }
+        } 
+
+        getPhotos()
+
+        console.log(photos)
+    }, [])
+
+    return(
+        <>
+            <h1>My Photo Album</h1>
+        </>
+    )
 }
 
 export default App
